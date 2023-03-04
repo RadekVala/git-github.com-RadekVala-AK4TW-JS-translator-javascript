55f0aa6879d45005b39dfd96a5989a7c90a18ec3
Radek Vala <radek.www@gmail.com>
2023-03-04 16:07:15 +0100

    Final commit - async/await fetch + localStorage

--- js/main.js
+++ js/main.js
@@ -2,9 +2,34 @@ const userInputEl = document.querySelector("#userInput");
 const btnTranslateEl = document.querySelector("#btnTranslate");
 const translationResultEl = document.querySelector('#translationResult');
 
+async function fetchData(userInput) {
+    try {
+      const response = await fetch(
+        `https://api.mymemory.translated.net/get?q=${userInput}&langpair=en|cs`
+      );
+      const data = await response.json();
+      console.log(data);
+      translationResultEl.innerHTML = data.responseData.translatedText;
+      
+      let history = [];
+      const historyLS = localStorage.getItem('history');
+      if(historyLS){
+        history = JSON.parse(historyLS);
+      }
+
+      history.push(data.responseData.translatedText);
+      localStorage.setItem('history', JSON.stringify(history));
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 btnTranslateEl.addEventListener("click", (event) => { 
     const userInput = encodeURIComponent(userInputEl.value);
 
+    fetchData(userInput);
+    /*
     fetch(`https://api.mymemory.translated.net/get?q=${userInput}&langpair=en|cs`)
     .then(response => response.json())
     .then(data => {
@@ -13,7 +38,8 @@ btnTranslateEl.addEventListener("click", (event) => {
         translationResultEl.innerHTML = data.responseData.translatedText;
     })
     .catch(error => console.error(error));
-
+    */
+    
 
 });
 
