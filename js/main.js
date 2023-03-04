4a69afdb4dbdfa7457ee54421f1fb68d97ca958a
Radek Vala <radek.www@gmail.com>
2023-03-04 15:46:33 +0100

    Basich communication with Rest using fetch and then

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const userInputEl = document.querySelector("#userInput");
+const btnTranslateEl = document.querySelector("#btnTranslate");
+const translationResultEl = document.querySelector('#translationResult');
+
+btnTranslateEl.addEventListener("click", (event) => { 
+    const userInput = encodeURIComponent(userInputEl.value);
+
+    fetch(`https://api.mymemory.translated.net/get?q=${userInput}&langpair=en|cs`)
+    .then(response => response.json())
+    .then(data => {
+        // Do something with the JSON data
+        console.log(data);
+    })
+    .catch(error => console.error(error));
+
+
+});
+
